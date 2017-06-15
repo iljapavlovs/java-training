1dbdf0f55031e96a8c353aa88907ef539e7e0046
njansone <natalija986@gmail.com>
2017-06-15 09:58:40 +0300

    15.06.2017.

---
+++ src/test/java/javaTraining/module3/Animal.java
@@ -0,0 +1,108 @@
+package javaTraining.module3;
+
+/*
+### Создайте более специализированные классы Animal - Lion and Elephant (** Наследование, отношения IS-A, Order of Calling Constructors, ключевое слово Super) **)
+1. Переопределить поведение и поля из класса Super (** Переопределение метода **)
+     1. Можно ли переопределить переменные экземпляра?
+2. Предоставляйте интерфейсы для написания полей чтения и чтения.
+3. Инициализируйте объекты Lion и Elephant и установите все переменные экземпляра.
+4. Вызвать все виды поведения
+*/
+
+class Animal {
+    protected int age;
+    protected int size;
+    protected String color;
+
+    //Animal constructor
+    Animal(int age, int size, String color){
+        this.age = age;
+        this.size = size;
+        this.color = color;
+    }
+
+
+
+    public void setAge(int age){
+        this.age = age;
+    }
+    public int getAge(){
+        return age;
+    }
+
+    public void setSize(int size){
+        this.size = size;
+    }
+    public int getSize(){
+        return size;
+    }
+
+    public void setColor(String color){
+        this.color=color;
+    }
+    public String getColor(){
+        return color;
+    }
+
+
+    //Animal eating
+    public void eating(){
+        System.out.println("Nyam nyam nyam");
+    }
+
+    //Animal sleeping
+    public void sleeping(){
+        System.out.println("ZzzzZZzzzzzZzZzZZzzzZzZ");
+    }
+
+    //Animal make a noise
+    public void makingANoise(){
+        System.out.println("BAM BAM CRASH SPLASH");
+    }
+
+
+    //Information about animal
+    public void information(){
+        System.out.println("Age: " + age);
+        System.out.println("Size: " + size);
+        System.out.println("Color: " + color);
+    }
+}
+
+
+
+
+class Lion extends Animal{
+    String name;
+    Lion(String name, int age, int size, String color) {
+        super(age, size, color);
+        this.name = name;
+    }
+    //Method Overriding
+    public void information(){
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Size: " + size);
+        System.out.println("Color: " + color);
+    }
+
+}
+
+
+
+
+class Elephant extends Animal{
+    String name;
+    Elephant(String name, int age, int size, String color) {
+        super(age, size, color);
+        this.name = name;
+    }
+    //Method Overriding
+    public void information(){
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Size: " + size);
+        System.out.println("Color: " + color);
+    }
+
+}
---
+++ src/test/java/javaTraining/module3/Main.java
@@ -0,0 +1,28 @@
+package javaTraining.module3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Lion lion = new Lion("Lion",3, 2, "White");
+        Elephant elephant= new Elephant("Elephant",10, 5, "Gray");
+
+        System.out.println("Lion:");
+        lion.information();
+        lion.eating();
+        lion.sleeping();
+        lion.makingANoise();
+        System.out.println();
+
+
+        System.out.println("Elephant:");
+        elephant.information();
+        elephant.sleeping();
+        elephant.eating();
+        elephant.makingANoise();
+        System.out.println();
+
+
+
+
+    }
+}
