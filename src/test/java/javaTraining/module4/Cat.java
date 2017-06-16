43289308816d23a6d8edab212ac6de4993821777
njansone <natalija986@gmail.com>
2017-06-16 10:39:51 +0300

    16.06.2017.

---
+++ src/test/java/javaTraining/module4/Cat.java
@@ -0,0 +1,8 @@
+package javaTraining.module4;
+
+
+public class Cat extends Animal {
+    public void makingANoise(){
+        System.out.println("Myau Myau Myau");
+    }
+}
---
+++ src/test/java/javaTraining/module4/Main.java
@@ -0,0 +1,11 @@
+package javaTraining.module4;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        dog.makingANoise();
+        cat.makingANoise();
+    }
+}
