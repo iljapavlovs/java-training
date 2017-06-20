da46a7411f823be9c032ff51cbe02cf357fdb168
njansone <natalija986@gmail.com>
2017-06-20 10:41:25 +0300

    20.06.2017.

--- src/test/java/javaTraining/module4/Animal.java
+++ src/test/java/javaTraining/module4/Animal.java
@@ -1,7 +1,7 @@
 package javaTraining.module4;
 
 
-class Animal implements WashingPlace.Washable{
+abstract class Animal implements WashingPlace.Washable{
 
     public void wash() {
         System.out.println(" Make Clean");
