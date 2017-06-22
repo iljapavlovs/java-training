05dd6fe31e8326eed759715e78a352e03ee7214c
njansone <natalija986@gmail.com>
2017-06-22 11:23:12 +0300

    Module 4, interface

--- src/test/java/javaTraining/module4/Animal.java
+++ src/test/java/javaTraining/module4/Animal.java
@@ -1,7 +1,7 @@
 package javaTraining.module4;
 
 
-abstract class Animal implements WashingPlace.Washable{
+abstract class Animal implements Washable{
 
     public void wash() {
         System.out.println(" Make Clean");
